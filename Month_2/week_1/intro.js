5273f55f145a37a2a7272201a6f26c39d2715039
Moses-main <moseschizaram8@gmail.com>
2025-05-05 14:07:26 +0100

    Add the code for month 2

---
+++ Month_2/week_1/intro.js
@@ -0,0 +1,47 @@
+// alert("This is an external javascript file")
+
+// Variables and Data types
+// Variables: Are containers that holds data
+
+// we use the keywords: (let, const and var) to declare our variables
+// e.g
+// mutable variable
+let name = "Moses";
+
+// constant variable
+const fieldOfStudy = "Computer Science";
+
+// Data types
+// This is the type of data that are being to our variables
+
+// String
+// Number
+// Boolean
+// Array
+// Object
+
+// Arithmentic Operators
+let A = 2;
+let B = 5;
+let C = 7;
+let D = 1;
+let F = 4;
+
+let sum = A + B; // addition
+let diff = B - A; // subtractio
+let quotient = C / A; // division
+let product = D * F; // multiplication
+
+//
+// alert(product);
+
+// interact with the make up
+// console.log(sum);
+
+// Expression
+let x = 3;
+let y = 2;
+
+// This is an expression
+let result = (x + y) * 2;
+alert(result);
