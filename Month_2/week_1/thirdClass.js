5aab28c822d6ee429e05bb21ee64bebe8beda6e3
Moses-main <moseschizaram8@gmail.com>
2025-05-13 19:33:54 +0100

    Add third class for week 7

---
+++ Month_2/week_1/thirdClass.js
@@ -0,0 +1,68 @@
+// alert("connected");
+
+let username = "Moses";
+
+// arrow function
+// const hello = (a) => {
+//   //   hello;
+// };
+
+// // normal function
+// function name() {
+//   // do something
+// }
+
+// declaring function
+// function handleAlert() {
+//   alert(`hello, ${username}`);
+// }
+
+// invoking function
+// handleAlert();
+
+// invoking an arrow function.
+// hello(a);
+
+// RETURN STATEMENT IN A FUNCTION
+
+//1
+function great(surname) {
+  return `Hello ${surname}`;
+}
+
+//2
+let greetings = great("Chioma");
+
+//3
+let firstPerson = great("Chioma");
+let secondPerson = great("Paul");
+let thirdPerson = great("Grace");
+let fourthPerson = great("Patrick");
+let fifthPerson = great("Mary");
+
+// IF ELSE STATEMENT
+
+// IF STATEMENT
+let a = 2;
+let b = 2;
+
+// if (a != b) {
+//   alert("The two numbers are not the same");
+// } else {
+//   alert("The two numbers are the same");
+// }
+
+// alert("a and b are the same") ? a == b : alert("The are not the same");
+//FOR Loop: Mostly used when the number of repetition is known
+
+// for (let digit = 1; digit <= 10; digit++) {
+//   alert(`The value of digit is ${digit}`);
+// }
+
+// WHILE LOOP: this is used when the number of repetition is not known
+
+let i = 0;
+while (i <= saleDepartment) {
+  alert(`This is the value of ${i}`);
+  i++;
+}
