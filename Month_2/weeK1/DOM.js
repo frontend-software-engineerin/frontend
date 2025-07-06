3f1966b889b80904818ac9dfa5cf93f139cbff7d
Moses-main <moseschizaram8@gmail.com>
2025-07-06 20:10:57 +0100

    Add the code for student_grade_checker

---
+++ Month_2/weeK1/DOM.js
@@ -0,0 +1,25 @@
+// byID
+// byClassname
+// queryselector
+
+// getElement by Id
+let heading_one = document.getElementById("title");
+
+heading_one.style.color = "red";
+
+// using the query selector
+
+// mutable
+let first_number = 2;
+
+first_number = 3;
+let second_number = 4;
+
+first_number + second_number; // 7
+
+// constant
+const third_number = 5;
+third_number = 7;
+
+let page_description = document.querySelector(".description");
+// page_description
